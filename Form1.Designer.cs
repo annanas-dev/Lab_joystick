07e749f985a486a1d2e506ccdc0fea1b98b7e711
annanas-dev <annanasidletskaia@icloud.com>
2024-04-22 10:30:45 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Lab_7_TP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(237, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 109);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "X+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(237, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 109);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "X-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(237, 199);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 109);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Стоп";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(364, 199);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 109);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "CW";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(110, 199);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 109);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "CCW";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(491, 199);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(121, 109);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "ZX";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(491, 84);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(121, 109);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Z+";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(491, 314);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(121, 109);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Z-";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(640, 364);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(345, 59);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Очистить";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(640, 429);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(168, 72);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Сохранить в файл";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(640, 54);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(345, 304);
+            this.listBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(433, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Режим:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(814, 429);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(171, 72);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "Выход";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 595);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_7_TP
+{
+    public partial class Form1 : Form
+    {
+
+        private int xCoord = 0;
+        private int zCoord = 0;
+        private int angle = 0;
+        private bool xMoving = false;
+        private bool zMoving = false;
+        private bool rotating = false;
+        private bool xMode = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            button9.Click += button9_Click;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            xMode = true;
+            label1.Text = "Режим: XT";
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = false;
+            button8.Enabled = false;
+        }
+
+        private void UpdateCoordinates()
+        {
+            listBox1.Items.Add($"X: {xCoord}, Z: {zCoord}, Угол: {angle}°");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!xMoving)
+            {
+                xMoving = true;
+                Timer xTimer = new Timer();
+                xTimer.Interval = 100;
+                xTimer.Tick += (s, ev) =>
+                {
+                    xCoord++;
+                    UpdateCoordinates();
+                    if (!xMoving)
+                        xTimer.Stop();
+                };
+                xTimer.Start();
+            }
+            else
+            {
+                xMoving = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!xMoving)
+            {
+                xMoving = true;
+                Timer xTimer = new Timer();
+                xTimer.Interval = 100;
+                xTimer.Tick += (s, ev) =>
+                {
+                    xCoord--;
+                    UpdateCoordinates();
+                    if (!xMoving)
+                        xTimer.Stop();
+                };
+                xTimer.Start();
+            }
+            else
+            {
+                xMoving = false;
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (!zMoving)
+            {
+                zMoving = true;
+                Timer zTimer = new Timer();
+                zTimer.Interval = 100;
+                zTimer.Tick += (s, ev) =>
+                {
+                    zCoord++;
+                    UpdateCoordinates();
+                    if (!zMoving)
+                        zTimer.Stop();
+                };
+                zTimer.Start();
+            }
+            else
+            {
+                zMoving = false;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (!zMoving)
+            {
+                zMoving = true;
+                Timer zTimer = new Timer();
+                zTimer.Interval = 100;
+                zTimer.Tick += (s, ev) =>
+                {
+                    zCoord--;
+                    UpdateCoordinates();
+                    if (!zMoving)
+                        zTimer.Stop();
+                };
+                zTimer.Start();
+            }
+            else
+            {
+                zMoving = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!rotating)
+            {
+                rotating = true;
+                Timer rotateTimer = new Timer();
+                rotateTimer.Interval = 100;
+                rotateTimer.Tick += (s, ev) =>
+                {
+                    angle += 10;
+                    if (angle >= 360)
+                        angle -= 360;
+                    UpdateCoordinates();
+                    if (!rotating)
+                        rotateTimer.Stop();
+                };
+                rotateTimer.Start();
+            }
+            else
+            {
+                rotating = false;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!rotating)
+            {
+                rotating = true;
+                Timer rotateTimer = new Timer();
+                rotateTimer.Interval = 100;
+                rotateTimer.Tick += (s, ev) =>
+                {
+                    angle -= 10;
+                    if (angle < 0)
+                        angle += 360;
+                    UpdateCoordinates();
+                    if (!rotating)
+                        rotateTimer.Stop();
+                };
+                rotateTimer.Start();
+            }
+            else
+            {
+                rotating = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            xMoving = false;
+            zMoving = false;
+            rotating = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            xMode = !xMode;
+            if (xMode)
+            {
+                label1.Text = "Режим: XT";
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+                button6.Enabled = true;
+                button7.Enabled = false;
+                button8.Enabled = false;
+            }
+            else
+            {
+                label1.Text = "Режим: ZX";
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = false;
+                button5.Enabled = false;
+                button6.Enabled = true;
+                button7.Enabled = true;
+                button8.Enabled = true;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); 
+            xCoord = 0; 
+            zCoord = 0; 
+            angle = 0; 
+            xMoving = false; 
+            zMoving = false; 
+            rotating = false; 
+
+            xMode = true;
+            label1.Text = "Режим: XT";
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            button7.Enabled = false;
+            button8.Enabled = false;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog.Title = "Сохранить данные";
+            saveFileDialog.FileName = "coordinates.txt"; 
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                try
+                {
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(filePath))
+                    {
+                        foreach (var item in listBox1.Items)
+                        {
+                            file.WriteLine(item.ToString());
+                        }
+                    }
+
+                    MessageBox.Show("Данные успешно сохранены", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка при сохранении файла: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                DialogResult result = MessageBox.Show("Хотите сохранить данные перед выходом?", "Сохранение данных", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    button10_Click(sender, e);
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
+
+            Application.Exit();
+        }
+    }
+}
